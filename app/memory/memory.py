5d04f2e82370faee873a86d6cbd4bdc5850c7e2c
Tanuj26Rajput <tanuj26rajput@gmail.com>
2026-03-24 15:57:40 +0530

    Update memory.py

--- app/memory/memory.py
+++ app/memory/memory.py
@@ -1,17 +1,19 @@
 from pymongo import MongoClient
 from datetime import datetime
+from app.graph.state import GraphState
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["synthra"]
-collection = client["sessions"]
+collection = db["sessions"]
 
 def save_sessions(session_id, state):
     data = {
         "session_id": session_id,
-        "query": state.query,
-        "plan": state.plan,
-        "report": state.final_report,
-        "sources": state.ranked_sources,
+        "state": state,
+        "query": state.get("query"),
+        "plan": state.get("plan"),
+        "report": state.get("final_report"),
+        "sources": state.get("ranked_sources"),
         "timestamp": datetime.utcnow()
     }
     collection.insert_one(data)
@@ -20,3 +22,9 @@ def load_history(session_id):
     history = list(collection.find({"session_id": session_id}))
     return history
 
+def load_last_state(session_id):
+    data = collection.find_one(
+        {"session_id": session_id},
+        sort = [("timestamp", -1)]
+    )
+    return GraphState(**data["state"])
